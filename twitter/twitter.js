db1660eb5e4b72f3249abfeabcba9f7d0278dc30
Isa Schottert <isaschottert@gmail.com>
2018-09-12 10:08:25 +0200

    Twitter widget

--- twitter/twitter.js
+++ twitter/twitter.js
@@ -1,3 +1,7 @@
 var pagina = document.getElementById("twitter");
 
-pagina.innerHTML = "<span>TWITTER</span>";
+pagina.innerHTML = `
+<a class="twitter-timeline" href="https://twitter.com/Windesheiminfo?ref_src=twsrc%5Etfw">
+Tweets by Windesheiminfo</a>
+`;
+//<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
