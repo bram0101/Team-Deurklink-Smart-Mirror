bc474ecf3f14f080b7f629f7ebffac09a82c8aaf
mdwilde <43200618+mdwilde@users.noreply.github.com>
2018-09-12 12:31:50 +0200

    ingevult scrippie
    ja is mooi nie

--- klok/klok.js
+++ klok/klok.js
@@ -1,3 +1,25 @@
 var pagina = document.getElementById("klok");
 
-pagina.innerHTML = "<span>KLOK</span>";
+function Updatetime(){
+    pagina.innerHTML = "<span>KLOK</span>";
+    var clock = "";
+    var date = new Date();
+    var hour = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    var hourtxt = hour + "";
+    var minutestxt = minutes + "";
+    var secondstxt = seconds + "";
+    if (hourtxt.length == 1){
+      hourtxt = "0" + hourtxt;
+    }
+    if (minutestxt.length == 1){
+
+      minutestxt = "0" + minutestxt;
+    }
+    if (secondstxt.length ==1){
+      secondstxt = "0" + secondstxt;
+    }
+    clock = hourtxt + ":" + minutestxt + ":" + secondstxt;
+   pagina.innerHTML = "<div>"+clock+"</div>"
+}
