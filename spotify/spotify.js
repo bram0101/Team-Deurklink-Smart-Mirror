82ce8d13b58a7f618a8818e74a7b57eb98567847
Isa Schottert <isaschottert@gmail.com>
2018-09-12 12:25:37 +0200

    sportify widget af

--- spotify/spotify.js
+++ spotify/spotify.js
@@ -1,3 +1,11 @@
 var pagina = document.getElementById("spotify");
 
-pagina.innerHTML = "<span>SPOTIFY</span>";
+pagina.innerHTML = `
+<div class="spotifywrapper">
+<iframe src="https://open.spotify.com/embed/user/spotify/playlist/37i9dQZF1DWSBi5svWQ9Nk"
+width="500"
+height="830"
+frameborder="0"
+allowtransparency="true"
+allow="encrypted-media"></iframe>
+</div>`;
