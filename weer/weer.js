78d599ee25205aca1df6de0626a05ff6b651061b
Airlina <Airlina@users.noreply.github.com>
2018-09-13 15:30:56 +0200

    Font aangepast en de layout verder aangepast, nieuwe icons toegevoegd.

--- weer/weer.js
+++ weer/weer.js
@@ -18,34 +18,34 @@ function fetchweer(){
 function updateweer(myJson) {
 
 var weergegevens = myJson['liveweer'][0];
-document.getElementById("weer_icon").src="weer/afbeeldingen/" + weergegevens['image'] + ".png";
-document.getElementById('weer_plaats').innerHTML="Weersverwachting voor: " + weergegevens['plaats'];
-document.getElementById('weer_temperatuur').innerHTML="Huidige temperatuur: " + weergegevens['temp'] + " C" + "&#730";;
-document.getElementById('weer_samenvatting').innerHTML="Huidige weeromstandigheden: " + weergegevens['samenv'];
-document.getElementById('weer_windrichting').innerHTML="Windrichting: " + weergegevens['windr'];
-document.getElementById('weer_windsnelheid').innerHTML="Windkracht: " + weergegevens['winds'] + " Bft";
-document.getElementById('weer_neerslagvandaag').innerHTML="Neerslagkans vandaag: " + weergegevens['d0neerslag'] + "%";
+document.getElementById("weer_icon").src="weer/afbeeldingen/" + weergegevens['image'] + ".svg";
+document.getElementById('weer_plaats').innerHTML="<b>Weersverwachting voor: </b>" + weergegevens['plaats'];
+document.getElementById('weer_temperatuur').innerHTML=weergegevens['temp'] + " C" + "&#730";;
+document.getElementById('weer_samenvatting').innerHTML=weergegevens['samenv'];
+document.getElementById('weer_windsnelheid').innerHTML=weergegevens['winds'] + " Bft";
+document.getElementById('weer_neerslagvandaag').innerHTML="Neerslagkans: " + weergegevens['d0neerslag'] + "%";
 
-document.getElementById('weer_plaats2').innerHTML="Weersverwachting voor morgen in: " + weergegevens['plaats'];
-document.getElementById("weer_icon_morgen").src="weer/afbeeldingen/" + weergegevens['d0weer'] + ".png";
-document.getElementById('weer_verwachting').innerHTML="Verwachting: " + weergegevens['verw'];
-document.getElementById('weer_neerslagmorgen').innerHTML="Neerslagkans morgen: " + weergegevens['d1neerslag'] + "%";
-document.getElementById('weer_maxtempmorgen').innerHTML="Max. Temperatuur morgen: " + weergegevens['d1tmax'] + " C" + "&#730";
-document.getElementById('weer_mintempmorgen').innerHTML="Min. Temperatuur morgen: " + weergegevens['d1tmin'] + " C" + "&#730";
+document.getElementById('weer_plaats2').innerHTML="<b>Morgen in: </b>" + weergegevens['plaats'];
+document.getElementById("weer_icon_morgen").src="weer/afbeeldingen/" + weergegevens['d0weer'] + ".svg";
+document.getElementById('weer_verwachting').innerHTML="<i>" + weergegevens['verw'] + "<i>";
+document.getElementById('weer_neerslagmorgen').innerHTML="<b>Neerslagkans: </b>" + weergegevens['d1neerslag'] + "%";
+document.getElementById('weer_maxtempmorgen').innerHTML="<b>Max: </b>" + weergegevens['d1tmax'] + " C" + "&#730";
+document.getElementById('weer_mintempmorgen').innerHTML="<b>Min: </b>" + weergegevens['d1tmin'] + " C" + "&#730";
 
-document.getElementById('weer_plaats3').innerHTML="Weersverwachting voor overmorgen in: " + weergegevens['plaats'];
-document.getElementById("weer_icon_overmorgen").src="weer/afbeeldingen/" + weergegevens['d1weer'] + ".png";
-document.getElementById('weer_neerslagovermorgen').innerHTML="Neerslagkans morgen: " + weergegevens['d2neerslag'] + "%";
-document.getElementById('weer_maxtempovermorgen').innerHTML="Max. Temperatuur morgen: " + weergegevens['d2tmax'] + " C" + "&#730";
-document.getElementById('weer_mintempovermorgen').innerHTML="Min. Temperatuur morgen: " + weergegevens['d2tmin'] + " C" + "&#730";
+document.getElementById('weer_plaats3').innerHTML="<b>Overmorgen in: </b>" + weergegevens['plaats'];
+document.getElementById("weer_icon_overmorgen").src="weer/afbeeldingen/" + weergegevens['d1weer'] + ".svg";
+document.getElementById('weer_neerslagovermorgen').innerHTML="<b>Neerslagkans: </b>" + weergegevens['d2neerslag'] + "%";
+document.getElementById('weer_maxtempovermorgen').innerHTML="<b>Max: </b>" + weergegevens['d2tmax'] + " C" + "&#730";
+document.getElementById('weer_mintempovermorgen').innerHTML="<b>Min: </b>" + weergegevens['d2tmin'] + " C" + "&#730";
 
-document.getElementById('weer_zonsopgang').innerHTML="Zonsopgang: " + weergegevens['sup'];
-document.getElementById('weer_zonsondergang').innerHTML="Zonsondergang: " + weergegevens['sunder'];
+document.getElementById('weer_zonsopgang').innerHTML="<b>Zonsopgang:</b> " + weergegevens['sup'];
+document.getElementById('weer_zonsondergang').innerHTML="<b>Zonsondergang:</b> " + weergegevens['sunder'];
 }
 //Einde custom code
 
 
 pagina.innerHTML = `<span></span>
+
 <!-- Weerlive vandaag-->
 <div id="grid-container" class="grid-container">
 
@@ -54,6 +54,7 @@ pagina.innerHTML = `<span></span>
 </div>
 <div id="plaats_locatie" class="weerinfo_plaats">
 <div id="weer_plaats"> </div>  <!-- Weerlive waarde: plaats-->
+<div id="weer_verwachting"> </div>  <!-- Weerlive waarde: verw-->
 </div>
 <div id="weerinfo_v" class="weerinfo_vandaag">
 <div id="weer_temperatuur"> </div> <!-- Weerlive waarde: temp-->
@@ -68,7 +69,6 @@ pagina.innerHTML = `<span></span>
 </div>
 <div id="weerinformatie_morgen" class="weerinfo_morgen">
 <div id="weer_plaats2"> </div> <!-- Weerlive waarde: plaats-->
-<div id="weer_verwachting"> </div>  <!-- Weerlive waarde: verw-->
 <div id="weer_neerslagmorgen"> </div> <!-- Weerlive waarde: d1neerslag-->
 <div id="weer_maxtempmorgen"> </div> <!-- Weerlive waarde: d1tmax-->
 <div id="weer_mintempmorgen"> </div> <!-- Weerlive waarde: d1tmin-->
@@ -86,7 +86,9 @@ pagina.innerHTML = `<span></span>
 </div>
 <!-- Weerlive waarde: Overige waardes-->
 <div id="zonsinformatie" class="overig">
+<img src="weer/afbeeldingen/zonsopgang.svg" class="zoninfo"></img>
 <div id="weer_zonsopgang"> </div> <!-- Weerlive waarde: sup-->
+<img src="weer/afbeeldingen/zonsondergang.svg" class="zoninfo"></img>
 <div id="weer_zonsondergang"> </div> <!-- Weerlive waarde: sunder-->
 </div>
 </div>
