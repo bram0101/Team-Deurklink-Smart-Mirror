eb827bb8d35c068112834651b9cada88176b3c2e
Vincent <vincentcdejong@hotmail.com>
2018-09-13 12:55:24 +0200

    Nieuws

--- nieuws/nieuws.js
+++ nieuws/nieuws.js
@@ -1,3 +1,16 @@
 var pagina = document.getElementById("nieuws");
 
-pagina.innerHTML = "<span>NIEUWS</span>";
+pagina.innerHTML = `<span>NIEUWS</span>
+
+<head>
+<div id="divRss"> </div>
+</head>
+
+`;
+$(document).ready(function(){
+$('#divRss').FeedEk({
+FeedUrl : 'http://www.nu.nl/rss/Algemeen',
+MaxCount : 5,
+ShowDesc : false,
+ShowPubDate : true
+}); });
