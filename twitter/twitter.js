4d90282e036c9f65b1635b22c4c483275f0300bf
Bram Stout <me@bramstout.nl>
2018-09-14 14:03:57 +0200

    Fix

--- twitter/twitter.js
+++ twitter/twitter.js
@@ -8,14 +8,14 @@ Tweets by Windesheiminfo</a>
 `;
 //<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
-function twitter_loop(){
-    pagina_twitter.innerHTML = `
-    <div class="twitterwrapper">
-    <a class="twitter-timeline" href="https://twitter.com/Windesheiminfo?ref_src=twsrc%5Etfw">
-    Tweets by Windesheiminfo</a>
-    </div>
-    `;
-    setTimeout(twitter_loop, 15000);
-}
-
-setTimeout(twitter_loop, 15000);
+// function twitter_loop(){
+//     pagina_twitter.innerHTML = `
+//     <div class="twitterwrapper">
+//     <a class="twitter-timeline" href="https://twitter.com/Windesheiminfo?ref_src=twsrc%5Etfw">
+//     Tweets by Windesheiminfo</a>
+//     </div>
+//     `;
+//     setTimeout(twitter_loop, 15000);
+// }
+//
+// setTimeout(twitter_loop, 15000);
