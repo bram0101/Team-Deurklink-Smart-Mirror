1b2d7186c38e214ffefb378824bb03f60360f11e
Bram Stout <me@bramstout.nl>
2018-09-14 10:35:14 +0200

    gedoe

--- rooster/rooster.js
+++ rooster/rooster.js
@@ -1,3 +1,17 @@
 var pagina = document.getElementById("rooster");
 
 pagina.innerHTML = "<span>ROOSTER</span>";
+
+var classId = "ICTM1p";
+var limit = 4;
+var eindtijd = new Date() / 1000 + 18 * 60 * 60;
+
+fetch("https://windesheimapi.azurewebsites.net/api/v1/Klas/" + classId + "/Les?$orderby=starttijd&$top=" + limit + "&$filter=eindtijd gt " + eindtijd + "L")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(JSON.stringify(myJson));
+}).catch(function(error){
+    console.error(error);
+});
